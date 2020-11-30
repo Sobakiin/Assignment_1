e0da2709e3858a3e064390e4f9f5f154a07a5687
Sobakiin <wgreen16@my.bcit.ca>
2020-11-30 14:00:23 -0800

    Increased the functionality of the dark theme button, made the placeholder a proper text area.

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,11 +1,22 @@
 
 
 let themeBtn = document.querySelector(".theme-mode")
+let saveBtn = document.querySelector(".save-key")
+let noteBtn = document.querySelector(".new-note")
+let cancelBtn = document.querySelector(".cancel-key")
+let textBox = document.querySelector(".text-box")
+
+
 themeBtn.addEventListener("click",darkTheme)
 function darkTheme(){
     let backdrop = document.querySelector("body")
     backdrop.style.backgroundColor="#2d2932"
-    
+    saveBtn.style.backgroundColor="darkgrey"
+    noteBtn.style.backgroundColor="darkgrey" 
+    cancelBtn.style.backgroundColor="darkgrey"
+    textBox.style.backgroundColor="black"
+    textBox.style.color="white"
+
     themeBtn.removeEventListener("click",darkTheme)
     themeBtn.addEventListener("click",lightTheme)
     themeBtn.style.backgroundColor="#ebebeb"
@@ -15,6 +26,11 @@ function darkTheme(){
 function lightTheme(){
     let backdrop = document.querySelector("body")
     backdrop.style.backgroundColor="#ebede4"
+    saveBtn.style.backgroundColor="green"
+    noteBtn.style.backgroundColor="azure" 
+    cancelBtn.style.backgroundColor="lightcoral"
+    textBox.style.backgroundColor="white"
+    textBox.style.color="black"
     
     themeBtn.removeEventListener("click",lightTheme)
     themeBtn.addEventListener("click",darkTheme)
