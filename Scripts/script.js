9ed2cf7b503295f8e4c75a4a88b8ec2281257434
Sobakiin <wgreen16@my.bcit.ca>
2020-12-01 14:04:03 -0800

    Modified HTML and CSS to be functional with the cript. Added to the script to make new note functional.

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,13 +1,33 @@
 
+{
+var themeBtn = document.querySelector(".theme-mode")
+var saveBtn = document.querySelector(".save-key")
+var noteBtn = document.querySelector(".new-note")
+var cancelBtn = document.querySelector(".cancel-key")
+var textBox = document.querySelector(".text-box")
+var noteList = document.querySelector(".note-holder")
+}
 
-let themeBtn = document.querySelector(".theme-mode")
-let saveBtn = document.querySelector(".save-key")
-let noteBtn = document.querySelector(".new-note")
-let cancelBtn = document.querySelector(".cancel-key")
-let textBox = document.querySelector(".text-box")
+function reformation(bool){
+    if(bool === 0){
+        saveBtn.style.visibility = "hidden"
+        cancelBtn.style.visibility = "hidden"
+        textBox.style.visibility = "hidden"
+    }
+    if(bool===1){
+        saveBtn.style.visibility = "visible"
+        cancelBtn.style.visibility = "visible"
+        textBox.style.visibility = "visible"
+    }
+}
 
+reformation(0)
+let noteStorage = []
+let count = 0
 
 themeBtn.addEventListener("click",darkTheme)
+noteBtn.addEventListener("click",startNote)
+
 function darkTheme(){
     let backdrop = document.querySelector("body")
     backdrop.style.backgroundColor="#2d2932"
@@ -23,6 +43,7 @@ function darkTheme(){
     themeBtn.innerHTML= "Light Mode"
     themeBtn.style.color="black"
 }
+
 function lightTheme(){
     let backdrop = document.querySelector("body")
     backdrop.style.backgroundColor="#ebede4"
@@ -37,4 +58,26 @@ function lightTheme(){
     themeBtn.style.backgroundColor="#2d2932"
     themeBtn.innerHTML= "Dark Mode"
     themeBtn.style.color="white"
+}
+
+function noteWriting(){
+    
+    let item = document.createElement("li")
+    let link = document.createElement("a")
+    
+    let  text = textbox.innerHTML
+    noteStorage.push(text)
+
+    link.appendChild(document.createTextNode(`Note ${count}`))
+    link.src = `#note-${count+1}`
+    item.appendChild(link)
+    noteList.appendChild(item)
+
+    count++
+    
+}
+
+function startNote(){
+console.log("*ding*")
+reformation(1)
 }
